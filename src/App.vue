e85dd2b17e182837393105debbedf659dc86e50c
Margarita Sergeeva <margaritk@inbox.ru>
2022-09-28 21:26:07 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <app-alert
+      v-if="alert"
+      text="Это очень важное сообщение!"
+      title="Внимание!"
+      closable
+      @close="alert = false"
+    ></app-alert>
+    <div class="card">
+      <button class="btn primary" @click="toggleAlert">
+        {{alert ? 'Скрыть' : 'Показать'}} сообщение
+      </button>
+    </div>
+    <app-block></app-block>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/extensions
+import AppAlert from '@/components/AppAlert';
+// eslint-disable-next-line import/extensions
+import AppBlock from '@/components/AppBlock';
+import alertMixin from '@/alertMixin';
+
+export default {
+  mixins: [alertMixin],
+  components: {
+    AppAlert,
+    AppBlock,
+  },
+};
+</script>
---
+++ src/components/AppBlock.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <app-alert
+      v-if="alert"
+      text="Это очень важное сообщение!"
+      title="Внимание!"
+      type="danger"
+      @close="alert = false"
+    ></app-alert>
+    <div class="card">
+      <button class="btn primary" @click="toggleAlert">
+        {{alert ? 'Скрыть' : 'Показать'}} сообщение
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/extensions
+import AppAlert from '@/components/AppAlert';
+import alertMixin from '@/alertMixin';
+
+export default {
+  mixins: [alertMixin],
+  components: {
+    AppAlert,
+  },
+};
+</script>
